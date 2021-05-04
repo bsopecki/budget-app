b7994bff939f57e7c8b39ffacf714e9dd9ca4781
Bartłomiej Sopecki <bartlomiej.sopecki@mobica.com>
2021-05-04 16:03:58 +0200

    add general balance

--- budget-app/src/components/GeneralBalance.js
+++ budget-app/src/components/GeneralBalance.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Statistic, StatisticLabel, StatisticValue } from 'semantic-ui-react'
 
 function GeneralBalance(props) {
+    let status
+    { if (props.balance < 0) { status = 'red' } status = 'green' }
     return (
-        <Statistic size="small">
+        <Statistic size="small" color={status}>
             <StatisticLabel>Your Balance</StatisticLabel>
-            <StatisticValue>{props.balance}</StatisticValue>
+            <StatisticValue >{props.balance}</StatisticValue>
         </Statistic>
     )
 }
