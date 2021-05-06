56ba9c00c71d8729a36a98f40b8e7ffebc2115bf
Bartłomiej Sopecki <bartlomiej.sopecki@mobica.com>
2021-05-06 11:13:41 +0200

    ad external file for data

--- budget-app/src/App.js
+++ budget-app/src/App.js
@@ -7,72 +7,16 @@ import HistoryHeader from './components/HistoryHeader'
 import HistoryRecord from './components/HistoryRecord'
 import MainForm from './components/Form'
 import FormHeader from './components/FormHeader'
+import { finances } from './finances'
 
 const App = () => {
-  const [records, setNewRecord] = useState([{
-    id: 1,
-    amount: 25,
-    category: 'bills',
-    description: 'cell phone',
-    status: 'red'
-  },
-  {
-    id: 2,
-    amount: 1750,
-    category: 'payouts',
-    description: 'job salary',
-    status: 'green'
-  },
-  {
-    id: 3,
-    amount: 250,
-    category: 'bills',
-    description: 'rent',
-    status: 'red'
-  },
-  {
-    id: 4,
-    amount: 188,
-    category: 'extra incomes',
-    description: 'lottery',
-    status: 'green'
-  },
-  {
-    id: 5,
-    amount: 149,
-    category: 'bills',
-    description: 'car insurance',
-    status: 'red'
-  },
-  {
-    id: 6,
-    amount: 95,
-    category: 'bills',
-    description: 'loan installment',
-    status: 'red'
-  },
-  {
-    id: 7,
-    amount: 75,
-    category: 'extra incomes',
-    description: 'maintenance',
-    status: 'green'
-  },
-  {
-    id: 8,
-    amount: 40,
-    category: 'clothes expenses',
-    description: 'new shoes',
-    status: 'red'
-  },
-  {
-    id: 9,
-    amount: 37,
-    category: 'bills',
-    description: 'electricity',
-    status: 'red'
-  }])
-  const amounts = (param) => records.filter(({ status }) => status === param).map(({ amount }) => amount).reduce((a, b) => a + b)
+  const initialRecords = finances
+  const [records, setNewRecord] = useState(initialRecords)
+  const amounts = (param) => {
+    records.filter(({ status }) => status === param)
+      .map(({ amount }) => amount)
+      .reduce((a, b) => a + b)
+  }
   const mainBalance = amounts('green') - amounts('red')
 
   //Add task
