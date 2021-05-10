bcfa2ddd20c060f2d4cca5abd6d75c4a55482f53
Bartłomiej Sopecki <bartlomiej.sopecki@mobica.com>
2021-05-10 16:03:15 +0200

    install router dependency and reorganize files structure

--- budget-app/src/App.js
+++ budget-app/src/App.js
@@ -1,41 +1,9 @@
-import { useState } from 'react'
-import { Container } from 'semantic-ui-react'
-import MainHeader from './components/MainHeader'
-import GeneralBalance from './components/GeneralBalance'
-import DividedBalance from './components/DividedBalance'
-import HistoryHeader from './components/HistoryHeader'
-import HistoryRecord from './components/HistoryRecord'
-import MainForm from './components/Form'
-import FormHeader from './components/FormHeader'
-import { finances } from './finances'
+import BudgetPage from './pages/BudgetPage'
 
 const App = () => {
-  const initialRecords = finances
-  const [records, setNewRecord] = useState(initialRecords)
-  const amounts = (param) => records.filter(({ status }) => status === param).map(({ amount }) => amount).reduce((a, b) => a + b)
-  const mainBalance = amounts('green') - amounts('red')
-
-  //Add record
-  const addFinanceRecord = (financeRecord) => {
-    const id = records.length + 1;
-    const newFinanceRecord = { id, ...financeRecord }
-    setNewRecord([...records, newFinanceRecord])
-  }
-  //Delete record
-  const deleteRecord = (id) => {
-    setNewRecord(records.filter((record) => record.id !== id))
-  }
 
   return (
-    <Container>
-      <MainHeader />
-      <GeneralBalance balance={mainBalance} />
-      <DividedBalance data={amounts} />
-      <HistoryHeader text='History' />
-      <HistoryRecord records={records} onDelete={deleteRecord} />
-      <FormHeader text='Add new transaction'></FormHeader>
-      <MainForm onAdd={addFinanceRecord} />
-    </Container>
+    <BudgetPage />
   )
 }
 
---
+++ budget-app/src/pages/BudgetPage.js
@@ -0,0 +1,43 @@
+import FormHeader from '../components/budget/FormHeader'
+import MainHeader from '../components/budget/MainHeader'
+import DividedBalance from '../components/budget/DividedBalance'
+import GeneralBalance from '../components/budget/GeneralBalance'
+import HistoryHeader from '../components/budget/HistoryHeader'
+import HistoryRecord from '../components/budget/HistoryRecord'
+import MainForm from '../components/budget/Form'
+import { Container } from 'semantic-ui-react'
+import { useState } from 'react'
+
+import { finances } from '../finances'
+
+const BudgetPage = () => {
+    const initialRecords = finances
+    const [records, setNewRecord] = useState(initialRecords)
+    const amounts = (param) => records.filter(({ status }) => status === param).map(({ amount }) => amount).reduce((a, b) => a + b)
+    const mainBalance = amounts('green') - amounts('red')
+
+    //Add record
+    const addFinanceRecord = (financeRecord) => {
+        const id = records.length + 1;
+        const newFinanceRecord = { id, ...financeRecord }
+        setNewRecord([...records, newFinanceRecord])
+    }
+    //Delete record
+    const deleteRecord = (id) => {
+        setNewRecord(records.filter((record) => record.id !== id))
+    }
+
+    return (
+        <Container>
+            <MainHeader />
+            <GeneralBalance balance={mainBalance} />
+            <DividedBalance data={amounts} />
+            <HistoryHeader text='History' />
+            <HistoryRecord records={records} onDelete={deleteRecord} />
+            <FormHeader text='Add new transaction'></FormHeader>
+            <MainForm onAdd={addFinanceRecord} />
+        </Container>
+    )
+}
+
+export default BudgetPage
